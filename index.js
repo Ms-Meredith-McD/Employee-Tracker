cb508c4dde5ca37370a80c91bca7ba7d7d9a6cbb
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-02-05 16:56:05 -0600

    updated my notes for future steps

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// TODO: Include packages needed for this application
+// Packages needed for the application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
 
-// TODO: Create an array of questions for user input
+// User choices
 
 const data = [
     {
@@ -23,15 +23,19 @@ const data = [
     }
 ]
 
+// when application starts present questions and possibly a line that says "Employee Management System"
 
 
 // write a GET function to show all departments
+// no tables to join
 
 // write a GET function to show all roles
 // job title, role id, department, salary
+// join with department table, foreign key is department ID
 
 // write a GET function to show all employees
 // first name, last name, job title, department, salary, manager
+// join with role table and department table, foreign keys are department ID and role ID
 
 // write a POST function to add a department
 // prompt for dept name
@@ -44,3 +48,4 @@ const data = [
 
 // write a PUT function to update an employee role
 // prompt to select an employee and update their new role
+// get the employee database, select employee by ID, (type in new role or select?)
