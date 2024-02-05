510a9e272e87a9880cc115bcb34a78e552e73b87
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-02-05 09:23:32 -0600

    added more data to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,14 +7,20 @@ VALUES  ('Finance'),
 INSERT INTO role (title, salary, department_id)
 VALUES  ('Accountant', 80000, 1),
         ('Developer', 100000, 2),
-        ('HR Manager', 95000, 3),
+        ('HR Director', 95000, 3),
+        ('CTO', 200000, 4),
+        ('CEO', 200000, 4),
         ('CFO', 200000, 4);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES  ('Sally', 'Johnson', 4, null),
+        ('Henry', 'Miller', 5, null),
+        ('Margaret', 'Samuels', 6, null);
 
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ('Joe', 'London', 2, 1),       
+VALUES  ('Joe', 'London', 2, 1),     
+        ('Mary', 'Jones', 3, 2),
+        ('Mitch', 'Lawson', 2, 4);
