084d495ed3c1cb173d601ebd57d0bc8abd22a8a6
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-02-07 13:36:44 -0600

    changed schema, reloaded schema and reseeded

--- db/schema.sql
+++ db/schema.sql
@@ -5,27 +5,26 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCES department (id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role (id),
-    manager_id INT
+    REFERENCES `role`(id),
+    manager_id INT NULL,
     FOREIGN KEY (manager_id)
-    REFERENCES employee (id) 
-
+    REFERENCES employee(id)
 );
